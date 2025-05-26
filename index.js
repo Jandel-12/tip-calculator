0509551bf4c6484e54241cc584a530dafa6a7b18
jandel-12 <jandelinson@gmail.com.noreply.github.com>
2025-05-26 20:16:48 +0800

    added the custom value

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const input = document.getElementById("input");
 const calculate= document.getElementById("calculate");
 const display = document.querySelector(".display");
+const customDisplay = document.getElementById("customInput");
+const tipOption = document.getElementById("tip-option");
+let totalTip = 0;
+
+tipOption.addEventListener("change", ()=>
+{
+    if(tipOption.value === "custom")
+    {
+        customDisplay.style.display = "block";
+    }
+    else{
+        customDisplay.style.display = "none"
+    }
+
+    
+});
+
 calculate.addEventListener("click", ()=>
 {
-    let inputValue = parseInt(input.value);
-    const optionValue = document.getElementById("tip-option").value;
-
-    let totalTip = parseFloat(inputValue * optionValue);
-
-    display.textContent = totalTip
+    if(tipOption.value === "custom")
+    {
+        totalTip = parseInt(customDisplay.value) + parseInt(input.value);
+    }
+    else
+    {
+        let inputValue = parseInt(input.value);
+        const optionValue = document.getElementById("tip-option").value;
+        totalTip = parseFloat(inputValue * optionValue);
+    }
     
-    
-})
+
+    display.textContent = `Bill:$ ${totalTip}`;
+});
